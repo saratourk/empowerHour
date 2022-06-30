e77e04c769ef2c262c541fbb89a7e1eaf78abccb
Sara Tourk <saratourk786@gmail.com>
2022-06-30 11:48:16 -0500

    dear martin!!

---
+++ dearMartinViewController.swift
@@ -0,0 +1,29 @@
+//
+//  dearMartinViewController.swift
+//  empowerHour
+//
+//  Created by Scholar on 6/30/22.
+//
+
+import UIKit
+
+class dearMartinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
